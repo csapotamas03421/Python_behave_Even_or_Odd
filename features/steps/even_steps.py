f2704c55871c0d1da77e63dd0cc3418b0490d863
Csapó Tamás <CsapoTamas@pg.local>
2026-01-16 11:35:36 +0100

    Scenario and steps definition

--- features/steps/even_steps.py
+++ features/steps/even_steps.py
@@ -1,21 +1,22 @@
 from behave import given, when, then
 # TODO: Importáld a number_checker modult a src mappából
+from src.number_checker import check_number
 
 
 # TODO: Implementáld a Given step-et
-# @given('the number is {number}')
-# def step_given_number(context, number):
-#     pass
+@given('the number is 4')
+def step_given_number(context):
+    context.number = 4
 
 
 # TODO: Implementáld a When step-et
 # Használd a check_number függvényt a src/number_checker.py fájlból!
-# @when('I check the number')
-# def step_when_check_number(context):
-#     pass
+@when('I check the number')
+def step_when_check_number(context):
+    context.result = check_number(context.number)
 
 
 # TODO: Implementáld a Then step-et
-# @then('the result should be "{expected}"')
-# def step_then_result(context, expected):
-#     pass
+@then('I should told "{expected}"')
+def step_then_result(context, expected):
+    assert context.result == expected, f'Elvárt érték: "{expected}", de a kapott érték: "{context.result}"'

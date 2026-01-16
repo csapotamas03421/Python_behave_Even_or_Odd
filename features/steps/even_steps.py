e5d353f40fd34ef3a762e385a21b67aac84ff761
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-01-16 09:47:30 +0100

    fájl

--- features/steps/even_steps.py
+++ features/steps/even_steps.py
@@ -1,21 +1,21 @@
 from behave import given, when, then
-# TODO: Importáld a number_checker modult a src mappából
+from src.number_checker import check_number
 
 
 # TODO: Implementáld a Given step-et
-# @given('the number is {number}')
-# def step_given_number(context, number):
-#     pass
+@given('the number is {number}')
+def step_given_number(context, number):
+    context.number = int(number)
 
 
 # TODO: Implementáld a When step-et
 # Használd a check_number függvényt a src/number_checker.py fájlból!
-# @when('I check the number')
-# def step_when_check_number(context):
-#     pass
+@when('I check the number')
+def step_when_check_number(context):
+    context.result = check_number(context.number)
 
 
 # TODO: Implementáld a Then step-et
-# @then('the result should be "{expected}"')
-# def step_then_result(context, expected):
-#     pass
+@then('the result should be "{expected}"')
+def step_then_result(context, expected):
+    assert context.result == expected, f'Expected {expected}, but got {context.result}'
